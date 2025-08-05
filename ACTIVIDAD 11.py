0eb2e14b97db97648a768742c8fb46a121ec8c90
jonan <heber2025sandoval@gmail.com>
2025-08-05 15:18:13 -0600

    ultimo

--- ACTIVIDAD 11.py
+++ ACTIVIDAD 11.py
@@ -1,3 +1,20 @@
 def quick_sort(lista):
     if len(lista) <= 1:
         return lista
+    else:
+        pivote = lista[0]
+        menores = [x for x in lista[1:] if x.lower() <= pivote]
+        mayores = [x for x in lista[1:] if x.lower() > pivote]
+        return quick_sort(menores) + [pivote] + quick_sort(mayores)
+n = int(input("cuantos nombres quiere ingresar"))
+nombres = []
+
+for i in range (n):
+    nombre=input("ingrese el nombre")
+    nombres.append(nombre)
+
+nombres_orden=quick_sort(nombres)
+
+print("lista de nombres ordenada")
+for nombre in nombres_orden:
+    print(nombre)
